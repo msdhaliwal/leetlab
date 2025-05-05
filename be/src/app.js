63d52aa289dc1c234286b5126ef0b22694033611
Manpreet Singh <msdhaliwal@outlook.in>
2025-05-05 07:25:30 +0530

    📦: prettier

--- be/src/app.js
+++ be/src/app.js
@@ -1,9 +1,11 @@
-import express from "express"
+import express from 'express';
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/ping', (req, res) => {
 	res.send('pong');
